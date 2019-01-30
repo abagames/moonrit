a0d10d4e11703b92cff3be5e2f4b25fad2a5844b
ABA Games <cs8k-cyu@asahi-net.or.jp>
2019-01-30 09:33:29 +0900

    Add target color

--- src/led.ts
+++ src/led.ts
@@ -15,6 +15,7 @@ const brightnesss = [0, 0.33, 0.66, 1];
 
 export class Led {
   color = { r: 0, g: 0, b: 0 };
+  targetColor = { r: 0, g: 0, b: 0 };
   brightness = 0;
   targetBrightness = 0;
 
@@ -22,7 +23,7 @@ export class Led {
 
   setColor(colorIndex: number) {
     const c = colors[colorIndex];
-    this.color = {
+    this.targetColor = {
       r: (c & 0xff0000) >> 16,
       g: (c & 0xff00) >> 8,
       b: c & 0xff
@@ -34,8 +35,14 @@ export class Led {
   }
 
   update() {
+    this.color.r += (this.targetColor.r - this.color.r) * 0.3;
+    this.color.g += (this.targetColor.g - this.color.g) * 0.3;
+    this.color.b += (this.targetColor.b - this.color.b) * 0.3;
     this.brightness += (this.targetBrightness - this.brightness) * 0.3;
-    if (this.color === 0 || this.brightness < 0.1) {
+    if (
+      this.color.r + this.color.g + this.color.b < 64 ||
+      this.brightness < 0.1
+    ) {
       return;
     }
     view.fillRect(
--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ function update() {
   for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
       const led = matrix.leds[x][y];
-      led.setColor(((x + Math.floor(ticks / 30)) % 7) + 1);
-      led.setBrightness(((x + y * 2 + Math.floor(ticks / 20)) % 3) + 1);
+      led.setColor((y + Math.floor(ticks / 30)) % 8);
+      led.setBrightness((x + y * 2 + Math.floor(ticks / 20)) % 4);
     }
   }
   matrix.update();
